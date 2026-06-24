815ed76d2b6195e788320326b4d872f02364e2a4
logicmindsbyparii <logicmindsbyparii@gmail.com>
2026-06-24 17:56:55 +0530

    Update PrivateRoute.tsx

--- src/components/admin/PrivateRoute.tsx
+++ src/components/admin/PrivateRoute.tsx
@@ -1,8 +1,13 @@
-import { Navigate } from 'react-router-dom';
+import type { ReactElement } from "react";
+import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const token = localStorage.getItem('token');
-  if (!token) return <Navigate to="/login" replace />;
+const PrivateRoute = ({ children }: { children: ReactElement }) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
   return children;
 };
 
